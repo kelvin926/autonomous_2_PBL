b686c3a6879dbecf866cd85c95c9dbb9446a4679
kelvin926 <kelvin@kakao.com>
2022-11-30 16:52:48 +0900

    12주차

--- 자율주행심화PBL_12주차_팀과제_사람인식.py
+++ 자율주행심화PBL_12주차_팀과제_사람인식.py
@@ -1,5 +1,3 @@
-
-#%%
 from pop import Pilot, AI
 
 cam = Pilot.Camera(width=224, height=224)
@@ -8,17 +6,33 @@ car = Pilot.AutoCar()
 object_follow = AI.Object_Follow(cam)
 object_follow.load_model()
 
-#%%
 while True:
     ret = object_follow.detect(index='person')
-
     if ret is not None:
         steer = ret['x'] * 4
         car.steering = 1 if steer > 1 else -1 if steer < -1 else steer
-        
-        if object_follow['size_rate'] < 0.20: #실제 이 값으로 대상이 멀어졌다고 판단하기 힘듬
+        size_value = ret['size_rate']
+        if (size_value <= 0.1):
+            car.forward(80)
+        elif (0.125 < size_value <= 0.175):
             car.forward(50)
+        elif (0.175 < size_value <= 0.25):
+            car.forward(30)
         else:
-            car.stop()
+            car.backward(80)
     else:
         car.stop()
+
+"""
+[과제]
+1. 사람이 이동 중 사라질 때 찾기 (상태변수를 따로 하나 만들기, 이동하는 중에 좌회전을 하고 있었는지, 
+우회전 하고 있었는지 저장한 다음, 사람이 사라지면 그 방향으로 조금 더 회전하기)
+Hint : 속도 부분들을 가변으로 주어야 함.
+못찾았을 땐 속도를 조금 더 높여야 함.
+
+2. 사람이 다가오면 뒤로 조금 물러서기
+Hint : 20%의 거리가 좀 멀음 (1m) 가까워지면 점점 %가 높아짐 (멀리 있을 땐 더 빠르게)
+사람 가까움의 최고 %는 30%, 대신 가까워지면 점점 느리게 주행해야 함.
+
+3. 후진 할 때 뒤에 장애물 탐지해서, 장애물이 있으면 멈추기 (Lidar, Sonar)
+"""
