3863c0eae38267612393163ddd8be054c865ba8f
kelvin926 <kelvin@kakao.com>
2022-12-07 16:31:57 +0900

    후방 감지

--- 자율주행심화PBL_12주차_팀과제_사람인식.py
+++ 자율주행심화PBL_12주차_팀과제_사람인식.py
@@ -62,36 +62,37 @@ def on_lidar(car, lidar):
 def gogo(car, object_follow):
     gogo.is_stop = False
     
-    ret = object_follow.detect(index='person')
-    if ret is not None: # 사람이 감지되었을 때
-        find_num = 0
-        ret_steer = ret['x'] * 4
-        real_steer = 1 if ret_steer > 1 else -1 if ret_steer < -1 else ret_steer # 위 Ret값에서 4를 곱한 값이 1 이상이면 1, 아니면 -1 / -1 이하이면 -1, 아니면 1 -> 결국 1 / -1 두 값 중 하나로만 간다는 것.
-        car.steering = real_steer
-        size_value = ret['size_rate'] # 감지 사이즈 %단위로 나옴.
-        if (size_value <= 0.1): # 아주 멀리서 감지되었을 때 (10% 이하)
-            car.forward(60)
-        elif (0.1 < size_value <= 0.15):
-            car.forward(40)
-        else: # 너무 가까울 때(15% 이상)
-            car.steering = (- real_steer)
-            if Rear_Raw <= 500: # 후방 라이다 값이 500mm보다 작을 때
-                car.stop()
-                print("후방 감지")
-                time.sleep(5)
-            else:
-                car.backward(60)
-                time.sleep(0.3) # 0.3초동안 강하게 후진
-    else: # 사람이 감지되지 않았을 때
-        if find_num < 3:
-            car.forward(40)
+    while not gogo.is_stop:
+        ret = object_follow.detect(index='person')
+        if ret is not None: # 사람이 감지되었을 때
+            find_num = 0
+            ret_steer = ret['x'] * 4
+            real_steer = 1 if ret_steer > 1 else -1 if ret_steer < -1 else ret_steer # 위 Ret값에서 4를 곱한 값이 1 이상이면 1, 아니면 -1 / -1 이하이면 -1, 아니면 1 -> 결국 1 / -1 두 값 중 하나로만 간다는 것.
             car.steering = real_steer
-            find_num += 1
-            time.sleep(0.5)
-        else: # 1초 이상 찾아봤는데 없을 때
-            car.steering = 0
-            car.stop()
-            # print("3초 이상 사람이 감지되지 않았습니다.")
+            size_value = ret['size_rate'] # 감지 사이즈 %단위로 나옴.
+            if (size_value <= 0.1): # 아주 멀리서 감지되었을 때 (10% 이하)
+                car.forward(60)
+            elif (0.1 < size_value <= 0.15):
+                car.forward(40)
+            else: # 너무 가까울 때(15% 이상)
+                car.steering = (- real_steer)
+                if Rear_Raw <= 1000: # 후방 라이다 값이 500mm보다 작을 때
+                    car.stop()
+                    print("후방 감지")
+                    time.sleep(5)
+                else:
+                    car.backward(60)
+                    time.sleep(0.3) # 0.3초동안 강하게 후진
+        else: # 사람이 감지되지 않았을 때
+            if find_num < 3:
+                car.forward(40)
+                car.steering = real_steer
+                find_num += 1
+                time.sleep(0.5)
+            else: # 1초 이상 찾아봤는데 없을 때
+                car.steering = 0
+                car.stop()
+                # print("3초 이상 사람이 감지되지 않았습니다.")
 
 
 def main():
