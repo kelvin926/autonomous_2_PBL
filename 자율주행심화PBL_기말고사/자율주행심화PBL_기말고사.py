626d7fc7f5e436c5954554f1d8ced38c7b490641
kelvin926 <kelvin@kakao.com>
2022-12-12 18:22:30 +0900

    semi_fin

--- 자율주행심화PBL_기말고사/자율주행심화PBL_기말고사.py
+++ 자율주행심화PBL_기말고사/자율주행심화PBL_기말고사.py
@@ -21,9 +21,17 @@
 7. 사람은 한 명만 있다고 가정함.
 """
 
+'''
+close.wav : 가까이 있을 때
+far.wav : 멀리 있을 때
+very_far.wav : 엄청 멀리있을 때
+ggam_nol.wav : 깜짝 놀랐을 때
+missing.wav : 사람이 사라졌을 때
+'''
+
 from pop import Pilot, AI, LiDAR
 import time
-# from playsound import playsound
+from playsound import playsound
 from multiprocessing import Process
 
 lidar = LiDAR.Rplidar() # Lidar 객체 생성
@@ -32,19 +40,18 @@ lidar.startMotor() # Lidar 모터 작동
 
 cam = Pilot.Camera(width=224, height=224) # 자동차 카메라 객체 생성
 car = Pilot.AutoCar() # 자동차 객체 생성
-# car._out_sound = None # 자동차 객체에 out_sound 변수 추가
+car._out_sound = None # 자동차 객체에 out_sound 변수 추가
 
 object_follow = AI.Object_Follow(cam) # Object_Follow 객체 생성 (카메라 가져옴)
 object_follow.load_model() # Object_Follow 모델 로드
 
 def out_sound(wav_file):
-    # p = Process(target=playsound, args=(wav_file,))
-    # p.start()
+    p = Process(target=playsound, args=(wav_file,))
+    p.start()
     # if (car._out_sound != None):
     #         car._out_sound.terminate()
     #         car._out_sound = None
-    # return p
-    print(wav_file)
+    return p
 
 # ---------- 각종 변수들 ---------- #
 Real_Steer = 0 # 실제 자동차에 반영될 조향값
@@ -62,7 +69,6 @@ Distance_Very_Close = 300 # 30cm
 while True: # 무한 반복
     ret = object_follow.detect(index='person') # 사람 감지
     if ret is not None: # 사람이 감지되었을 때
-        # find_num = 0
         ret_steer = ret['x'] * 4 # 사람 감지 좌표값(박스의 정 중앙)을 4로 곱함.
         Real_Steer = 1 if ret_steer > 1 else -1 if ret_steer < -1 else ret_steer # 위 Ret값에서 4를 곱한 값이 1 이상이면 1, 아니면 -1 / -1 이하이면 -1, 아니면 1 -> 결국 1 / -1 두 값 중 하나로만 간다는 것.
         car.steering = Real_Steer # 위에서 구한 실제 조향 값 대입.
@@ -70,11 +76,11 @@ while True: # 무한 반복
         
         if (Size_value <= Size_Far): # 아주 멀리서 감지되었을 때
             car.forward(Speed_Middle) # 속도를 보통으로 함
-            out_sound("반가워서 짖는 소리")
+            out_sound("very_far.wav")
         
         elif (Size_Far < Size_value <= Size_Half): # 멀리서 감지되었을 때
             car.forward(Speed_Slow) # 속도를 느리게 함.
-            out_sound("산책할 때 헥헥대는 소리")
+            out_sound("far.wav")
         
         else: # 너무 가까울 때(15% 이상)
             global Rear_Raw
@@ -86,19 +92,18 @@ while True: # 무한 반복
                     Rear_Raw = v[1] # 후방 거리값
                     print(Rear_Raw) # 후방 거리값 출력
             
-            if ((Distance_Very_Close < Rear_Raw) and (Rear_Raw <= Distance_Close)): # 약간의 공간은 있음 -> 실제로는 생각보다 가까이 멈추기 때문에(가속도 때문), 조금 더 높여야 할 필요가 있음. (수정 필요)
+            if ((Distance_Very_Close < Rear_Raw) and (Rear_Raw <= Distance_Close)): # 약간의 공간은 있음
                 car.backward(Speed_Slow) # 느리게 후진
-                out_sound("무서워서 낑낑거리는 소리 + 물러서는 소리")
+                out_sound("close.wav")
             
             elif (Rear_Raw <= Distance_Very_Close): # 완전 공간 없음
                 car.stop() # 후방에 장애물이 감지되었기 때문에, 정지
-                out_sound("완전 무서워하는 소리 + 짖으면서..?")
+                out_sound("ggam_nol.wav")
             
             else: # 후방에 장애물이 감지되지 않았을 때
                 car.backward(Speed_Middle) # 계속 후진
-                out_sound("낑낑대는 소리 + 빠르게 물러서는 소리")
-                # time.sleep(0.3) # 0.3초동안 후진 (매끄럽게 수정 필요)
+                out_sound("close.wav")
 
     else: # 사람이 감지되지 않았을 때 -> 모델에 따라 행동을 변경해야 함.
         car.stop() # 일단 정지 -> 모델에 따라 행동을 변경해야 함.
-        out_sound("사람 찾는 소리")
+        out_sound("missing.wav")
