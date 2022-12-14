376e2a6c44031c89c0f2ea9bec45b05c705077d7
kelvin926 <kelvin@kakao.com>
2022-12-14 14:47:39 +0900

    fi

--- 자율주행심화PBL_기말고사/자율주행심화PBL_기말고사.py
+++ 자율주행심화PBL_기말고사/자율주행심화PBL_기말고사.py
@@ -58,60 +58,56 @@ Size_Half = 0.15 # 15%
 Distance_Close = 500 # 50cm
 Distance_Very_Close = 300 # 30cm
 
-out_sound = "very_far"
+out_sound = "very_far.wav"
 
-def go(nope):
-    while True: # 무한 반복
-        global out_sound
-        ret = object_follow.detect(index='person') # 사람 감지
-        if ret is not None: # 사람이 감지되었을 때
-            ret_steer = ret['x'] * 4 # 사람 감지 좌표값(박스의 정 중앙)을 4로 곱함.
-            Real_Steer = 1 if ret_steer > 1 else -1 if ret_steer < -1 else ret_steer # 위 Ret값에서 4를 곱한 값이 1 이상이면 1, 아니면 -1 / -1 이하이면 -1, 아니면 1 -> 결국 1 / -1 두 값 중 하나로만 간다는 것.
-            car.steering = Real_Steer # 위에서 구한 실제 조향 값 대입.
-            Size_value = ret['size_rate'] # 감지 사이즈 %단위로 나옴.
-            
-            if (Size_value <= Size_Far): # 아주 멀리서 감지되었을 때
-                car.forward(Speed_Middle) # 속도를 보통으로 함
-                out_sound = "very_far.wav"
-            
-            elif (Size_Far < Size_value <= Size_Half): # 멀리서 감지되었을 때
-                car.forward(Speed_Slow) # 속도를 느리게 함.
-                out_sound = "far.wav"
-            
-            else: # 너무 가까울 때(15% 이상)
-                global Rear_Raw
-                Rear_Raw = 0 # Lidar 후방 거리값
-                car.steering = (- Real_Steer) # 후진하는 동안에는 반대 방향으로 조향
-                Raw = lidar.getVectors() # Lidar 값을 Raw로 받아옴
-                for v in Raw:
-                    if (v[0] >= 360 - 185 and v[0] <= 360 - 175): # 후방 10도
-                        Rear_Raw = v[1] # 후방 거리값
-                        print(Rear_Raw) # 후방 거리값 출력
-                
-                if ((Distance_Very_Close < Rear_Raw) and (Rear_Raw <= Distance_Close)): # 약간의 공간은 있음
-                    car.backward(Speed_Slow) # 느리게 후진
-                    out_sound = "close.wav"
-                
-                elif (Rear_Raw <= Distance_Very_Close): # 완전 공간 없음
-                    car.stop() # 후방에 장애물이 감지되었기 때문에, 정지
-                    out_sound = "ggam_nol.wav"
-                
-                else: # 후방에 장애물이 감지되지 않았을 때
-                    car.backward(Speed_Middle) # 계속 후진
-                    out_sound = "close.wav"
-
-        else: # 사람이 감지되지 않았을 때 -> 모델에 따라 행동을 변경해야 함.
-            car.stop() # 일단 정지 -> 모델에 따라 행동을 변경해야 함.
-            out_sound = "missing.wav"
-
-def say(out_sound):
+def say(nope):
     while True:
-        playsound(str(out_sound)+".wav")
-        time.sleep(1)
-    
+        playsound(out_sound)
+        time.sleep(0.5)
 
-g = Process(target = go, args=(None,)).start()
-s = Process(target = say, args=(out_sound,)).start()
+s = Process(target = say, args=(None,)).start()
+
+while True: # 무한 반복
+    ret = object_follow.detect(index='person') # 사람 감지
+    if ret is not None: # 사람이 감지되었을 때
+        ret_steer = ret['x'] * 4 # 사람 감지 좌표값(박스의 정 중앙)을 4로 곱함.
+        Real_Steer = 1 if ret_steer > 1 else -1 if ret_steer < -1 else ret_steer # 위 Ret값에서 4를 곱한 값이 1 이상이면 1, 아니면 -1 / -1 이하이면 -1, 아니면 1 -> 결국 1 / -1 두 값 중 하나로만 간다는 것.
+        car.steering = Real_Steer # 위에서 구한 실제 조향 값 대입.
+        Size_value = ret['size_rate'] # 감지 사이즈 %단위로 나옴.
+        
+        if (Size_value <= Size_Far): # 아주 멀리서 감지되었을 때
+            car.forward(Speed_Middle) # 속도를 보통으로 함
+            out_sound = "very_far.wav"
+        
+        elif (Size_Far < Size_value <= Size_Half): # 멀리서 감지되었을 때
+            car.forward(Speed_Slow) # 속도를 느리게 함.
+            out_sound = "far.wav"
+        
+        else: # 너무 가까울 때(15% 이상)
+            global Rear_Raw
+            Rear_Raw = 0 # Lidar 후방 거리값
+            car.steering = (- Real_Steer) # 후진하는 동안에는 반대 방향으로 조향
+            Raw = lidar.getVectors() # Lidar 값을 Raw로 받아옴
+            for v in Raw:
+                if (v[0] >= 360 - 185 and v[0] <= 360 - 175): # 후방 10도
+                    Rear_Raw = v[1] # 후방 거리값
+                    print(Rear_Raw) # 후방 거리값 출력
+            
+            if ((Distance_Very_Close < Rear_Raw) and (Rear_Raw <= Distance_Close)): # 약간의 공간은 있음
+                car.backward(Speed_Slow) # 느리게 후진
+                out_sound = "close.wav"
+            
+            elif (Rear_Raw <= Distance_Very_Close): # 완전 공간 없음
+                car.stop() # 후방에 장애물이 감지되었기 때문에, 정지
+                out_sound = "ggam_nol.wav"
+            
+            else: # 후방에 장애물이 감지되지 않았을 때
+                car.backward(Speed_Middle) # 계속 후진
+                out_sound = "close.wav"
+                
+    else: # 사람이 감지되지 않았을 때 -> 모델에 따라 행동을 변경해야 함.
+        car.stop() # 일단 정지 -> 모델에 따라 행동을 변경해야 함.
+        out_sound = "missing.wav"
 
 
 # def out_sound(wav_file):
